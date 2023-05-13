6c8a34eb7b21f14f273a2c6679bbd81226d79cc6
Marcos Lisboa <lisboamab@gmail.com>
2023-05-12 22:37:06 -0300

    feat: add mining_type input for coin form

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,15 @@ coins = [
     acronym: "BTC",
     url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
     active: true,
-    mining_type_id: 1
+    mining_type: MiningType.find_by(acronym: 'PoW')
   },
   {
     short_description: "Dogecoin",
     long_description: "Dogecoin is a cryptocurrency, like Bitcoin or Ethereum—although it's a very different animal than either of these popular coins. Dogecoin was originally created at least in part as a lighthearted joke for crypto enthusiasts, and took its name from a once-popular meme.",
     acronym: "DOGE",
     url_image: "https://upload.wikimedia.org/wikipedia/pt/d/d0/Dogecoin_Logo.png",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoW')
   },
   {
     short_description: "Ethereum",
@@ -28,42 +29,47 @@ coins = [
     acronym: "ETH",
     url_image: "https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/ethereum-eth-icon.png",
     active: true,
-    mining_type_id: 1
+    mining_type: MiningType.find_by(acronym: 'PoW')
   },
   {
     short_description: "Litecoin",
     long_description: "Litecoin is a peer-to-peer cryptocurrency that enables instant, near-zero cost payments to anyone in the world. It is an open-source project released in 2011 and is often considered as the silver to Bitcoin's gold.",
     acronym: "LTC",
     url_image: "https://cryptologos.cc/logos/litecoin-ltc-logo.png?v=002",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoW')
   },
   {
     short_description: "Cardano",
     long_description: "Cardano is a blockchain platform that aims to provide a more secure and sustainable infrastructure for the development of decentralized applications and smart contracts. It emphasizes academic research and peer-reviewed methodologies.",
     acronym: "ADA",
     url_image: "https://cryptologos.cc/logos/cardano-ada-logo.png",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoS')
   },
   {
     short_description: "Polkadot",
     long_description: "Polkadot is a multi-chain network that enables different blockchains to interoperate and share information in a secure and scalable way. It aims to create a decentralized web where independent blockchains can seamlessly connect and interact.",
     acronym: "DOT",
     url_image: "https://cryptologos.cc/logos/polkadot-new-dot-logo.png",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoS')
   },
   {
     short_description: "Ripple",
     long_description: "Ripple is a digital payment protocol and cryptocurrency that facilitates fast, low-cost international money transfers. It aims to provide a seamless experience for sending money globally, leveraging blockchain technology and a consensus algorithm.",
     acronym: "XRP",
     url_image: "https://cryptologos.cc/logos/xrp-xrp-logo.png",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoS')
   },
   {
     short_description: "Binance Coin",
     long_description: "Binance Coin is a digital currency issued by the Binance cryptocurrency exchange. It can be used to pay for transaction fees on the Binance exchange or to participate in token sales offered on the Binance Launchpad.",
     acronym: "BNB",
     url_image: "https://cryptologos.cc/logos/binance-coin-bnb-logo.png?v=002",
-    active: true
+    active: true,
+    mining_type: MiningType.find_by(acronym: 'PoS')
   }
 ]
 
